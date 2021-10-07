492c4acef6e16195149a1d2a7c85b83072ef862e
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-07 19:18:37 -0400

    Added route to serve stats.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,4 +4,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
 };
