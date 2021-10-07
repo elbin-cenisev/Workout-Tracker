bdd26891fba7fd35a7007418ce70139b5e8094b6
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-07 19:21:02 -0400

    Added exercise route to serve exercise.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,4 +7,7 @@ module.exports = function(app) {
   app.get("/stats", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
 };
