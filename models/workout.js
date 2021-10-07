d72ed38e7067d15ceead96d7e6a50c1e7030f549
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-07 19:04:50 -0400

    Built workout model according to seeds

---
+++ models/workout.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var workoutSchema = new Schema({
+    day: { type: Date, required: true },
+    exercises: [
+        {
+            name: {
+                type: String,
+                required: "Enter the name of the exercise"
+            },
+            type: {
+                type: String,
+                required: "Enter the type of the exercise"
+            },
+            duration: {
+                type: Number,
+                required: "Enter the duration for the exercise"
+            },
+            weight: {
+                type: Number,
+                required: "Enter a weight"
+            },
+            reps: {
+                type: Number,
+                required: "Enter the amount of reps for the exercise"
+            },
+            sets: {
+                type: Number,
+                required: "Enter the amount of sets for the exercise"
+            },
+        }
+    ],
+});
+
+var Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
