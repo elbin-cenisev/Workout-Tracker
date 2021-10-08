78282d72330c66813c4b5777363fc82ebc0c646e
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-07 23:20:59 -0400

    Past workouts now include total duration field

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,14 @@ const Workout = require("../models/workout.js");
 module.exports = function (app) {
     // Get past workouts
     app.get("/api/workouts", function (req, res) {
-        Workout.find({})
+        Workout.aggregate([
+
+            // 1. Find all workouts
+            { $match: {} },
+
+            // 2. Add the new totalDuration field which is the sum of all exercise durations
+            { $addFields: { totalDuration: { $sum: "$exercises.duration" } } }
+        ])
             .then(dbWorkout => {
                 res.json(dbWorkout);
             })
@@ -21,5 +28,16 @@ module.exports = function (app) {
             .catch(err => {
                 res.status(400).json(err);
             });
+    });
+
+    // Add an exercise
+    app.put("/api/workouts/:id", function (req, res) {
+        let exercise = req.body;
+        Workout.updateOne(
+            { _id: req.params.id },
+            { $push: { exercises: exercise } }
+        ).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        });
     });
 };
