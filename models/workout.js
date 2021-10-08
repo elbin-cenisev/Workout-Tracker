3ac3db91096e08a202a686c0b3ada014e4f6f887
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-07 21:20:49 -0400

    Removed "require" statements for empty Workout

--- models/workout.js
+++ models/workout.js
@@ -7,27 +7,27 @@ var workoutSchema = new Schema({
         {
             name: {
                 type: String,
-                required: "Enter the name of the exercise"
+                // required: "Enter the name of the exercise"
             },
             type: {
                 type: String,
-                required: "Enter the type of the exercise"
+                // required: "Enter the type of the exercise"
             },
             duration: {
                 type: Number,
-                required: "Enter the duration for the exercise"
+                // required: "Enter the duration for the exercise"
             },
             weight: {
                 type: Number,
-                required: "Enter a weight"
+                // required: "Enter a weight"
             },
             reps: {
                 type: Number,
-                required: "Enter the amount of reps for the exercise"
+                // required: "Enter the amount of reps for the exercise"
             },
             sets: {
                 type: Number,
-                required: "Enter the amount of sets for the exercise"
+                // required: "Enter the amount of sets for the exercise"
             },
         }
     ],
