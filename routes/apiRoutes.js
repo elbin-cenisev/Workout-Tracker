3772259d36aa7d1a219ae3e93cdc700cc731d998
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-07 21:21:00 -0400

    Added route to create Workout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,20 @@
 const Workout = require("../models/workout.js");
 
 module.exports = function (app) {
+    // Get past workouts
     app.get("/api/workouts", function (req, res) {
         Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
+
+    // Create new workout
+    app.post("/api/workouts", function (req, res) {
+        Workout.create(req.body)
             .then(dbWorkout => {
                 res.json(dbWorkout);
             })
