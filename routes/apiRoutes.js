a0687c6b532bca8a9bbd895829ce344fa7ea0cf4
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-07 21:15:43 -0400

    Route added: Finds past workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,13 @@
-module.exports = function(app) {
+const Workout = require("../models/workout.js");
+
+module.exports = function (app) {
+    app.get("/api/workouts", function (req, res) {
+        Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    });
 };
